90b5edc66f7ff205b650bc04725ea472c87c2825
Topolev <i.topolev.vladimir@gmail.com>
2017-05-15 16:28:49 +0300

    Chapter02

--- app/List.jsx
+++ app/List.jsx
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Card from './Card';
+import marked from 'marked';
 
 class List extends Component {
-    render(){
-        var cards = this.props.cards.map((card) =>{
-            return <Card id={card.id}
+    render() {
+        var cards = this.props.cards.map((card) => {
+            return <Card key={card.id}
+                         id={card.id}
                          title={card.title}
                          description={card.description}
-                         tasks = {card.tasks} />
+                         color={card.color}
+                         tasks={card.tasks}/>
         })
         return (
             <div className="list">
